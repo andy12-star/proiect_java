db67c5319e9089f91c85968f04492e8381f1317b
AndreeaIonita <ionitaandy@gmail.com>
2025-05-26 22:43:36 +0300

    Add config for student-service

--- student-service/src/main/java/studentservice/StudentServiceApplication.java
+++ student-service/src/main/java/studentservice/StudentServiceApplication.java
@@ -2,8 +2,9 @@ package studentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class StudentServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudentServiceApplication.class, args);
