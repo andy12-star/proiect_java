aac300c98bb25bfd2745115ab0034e7aa37c3fa6
AndreeaIonita <ionitaandy@gmail.com>
2025-05-28 00:32:25 +0300

    p3

--- enrollment-service/src/main/java/enrollmentservice/EnrollmentServiceApplication.java
+++ enrollment-service/src/main/java/enrollmentservice/EnrollmentServiceApplication.java
@@ -2,8 +2,9 @@ package enrollmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class EnrollmentServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EnrollmentServiceApplication.class, args);
