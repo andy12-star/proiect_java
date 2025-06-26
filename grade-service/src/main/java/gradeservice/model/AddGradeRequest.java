7e1d82ecac7d313c7f2d9a1139ad1c8b759a8acd
AndreeaIonita <ionitaandy@gmail.com>
2025-06-26 14:05:21 +0300

    modificare la grade model

--- grade-service/src/main/java/gradeservice/model/AddGradeRequest.java
+++ grade-service/src/main/java/gradeservice/model/AddGradeRequest.java
@@ -1,12 +1,8 @@
 package gradeservice.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class AddGradeRequest {
     private Long studentId;
     private Long courseId;
--- grade-service/src/main/java/gradeservice/model/Grade.java
+++ grade-service/src/main/java/gradeservice/model/Grade.java
@@ -5,12 +5,10 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Range;
 
 @Data
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "grades")
